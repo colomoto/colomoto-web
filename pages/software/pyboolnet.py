31b73292e9c6ce0f0a473797bfe91fc102a0de29
Aurelien Naldi <aurelien.naldi@gmail.com>
2016-08-25 15:50:18 +0200

    Replace boolnetfixpoints with pyboolnet

---
+++ pages/software/pyboolnet.py
@@ -0,0 +1,27 @@
+.. title: PyBoolNet
+.. tags: tools, related-groups
+.. description: Python API for the analysis of Boolean networks
+.. link: 
+.. type: text
+.. website: https://github.com/hklarner/PyBoolNet
+.. related-groups: fu-berlin
+.. formats: 
+.. features:
+
+
+.. tool_header::
+
+
+PyBoolNet is a Python package for the generation, modification
+and analysis of Boolean networks. The current version is
+available at https://github.com/hklarner/PyBoolNet/releases.
+
+For the manual, a reference and tutorials see the PyBoolNet
+manual. For questions and suggestions do not hestitate to open
+issues or contact hannes.klarner at fu-berlin.de
+
+.. ref:: Klarner2016
+
+
+.. tool_info::
+
